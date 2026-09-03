f07512d3631a26245bf799a4d7576c9c6c683786
dukke89 <david.benitez@milicic.com.ar>
2026-09-03 12:59:51 -0300

    actualizacion 03/09

--- js/last-update.js
+++ js/last-update.js
@@ -1,12 +1,12 @@
 // Editá SOLO este archivo para cambiar la fecha mostrada en el header.
-window.LAST_UPDATE = "31/08/2026";
+window.LAST_UPDATE = "03/09/2026";
 
 let cb = sessionStorage.getItem('mi_cache_buster');
 if (!cb) {
   cb = new Date().getTime();
   sessionStorage.setItem('mi_cache_buster', cb);
 }
-window.CACHE_BUSTER = cb + "_v10_periodo_corto";
+window.CACHE_BUSTER = cb + "_v11_periodo_corto";
 
 window.forceRefreshData = function() {
   sessionStorage.removeItem('mi_cache_buster');
@@ -17,3 +17,5 @@ window.forceRefreshData = function() {
     window.location.reload();
   }
 };
+
+
